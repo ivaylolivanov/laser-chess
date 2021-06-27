42be109077484f96a17993f02128b90319733209
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-06-28 00:24:54 +0300

    Add projectile script
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    private int damage;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Unit targetUnit = collision.gameObject.GetComponent<Unit>();
+        if(targetUnit != null) {
+            targetUnit.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void FireAt(Vector3 destination, int damage) {
+        this.damage = damage;
+
+        Vector3Int destinationInt = Vector3Int.FloorToInt(destination);
+        Vector3 direction = (destinationInt - transform.position).normalized;
+        Vector3Int directionInt = Vector3Int.RoundToInt(direction);
+
+        StartCoroutine(Move(directionInt));
+    }
+
+    private IEnumerator Move(Vector3Int direction) {
+        for(;;) {
+            Vector3Int currentPosition = Vector3Int.FloorToInt(
+                transform.position
+            );
+            Vector3Int newPosition = currentPosition + direction;
+
+            transform.position = newPosition;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
