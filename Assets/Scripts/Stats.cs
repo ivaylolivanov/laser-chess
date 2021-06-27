ca01d37457f70ec60406b13e7812eb0009ea3133
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-06-28 00:20:48 +0300

    Reorganize Unit stats into Stats
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Stats", menuName = "Stats", order = 1)]
+public class Stats : ScriptableObject {
+    public int maxHealth;
+    public int attackPower;
+    [Range(1, Utils.MAX_STEPS)] public int stepsLimit;
+    [Range(1, Utils.MAX_STEPS)] public int attackLimit;
+    public float intervalBetweenSteps;
+}
